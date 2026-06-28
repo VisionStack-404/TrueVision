f8cab2de71ac769bfb962c2be0eb8fbbc1c33f79
Varun <189742027+VisionStack-404@users.noreply.github.com>
2026-06-28 21:53:58 +0530

    clean up code

--- backend/test_api.py
+++ backend/test_api.py
@@ -3,7 +3,7 @@ import requests
 # YOUR EC2 instance URL
 url = "http://3.238.89.41:8000/upload/"
 
-# 🔥 VIDEO FILE (LOCAL)
+#  VIDEO FILE (LOCAL)
 file_path = "video.mp4"
 
 with open(file_path, "rb") as f:
